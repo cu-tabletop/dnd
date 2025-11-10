cc3ce42371dd7ab71cb6eac3c11bd51984605741
Igor <igor.mamaev1@gmail.com>
2025-11-10 16:31:51 +0300

    Ну вот база первых двух экранов

--- bot/handlers/mainmenu.py
+++ bot/handlers/mainmenu.py
@@ -16,16 +16,58 @@ router.include_router(
             Const("Это главное меню бота"),
             Column(
                 Button(
-                    Const("Кампании"), on_click=..., id="mainmenucampaigns"
+                    Const("Кампании"),
+                    on_click=lambda c, b, m: m.switch_to(MainMenu.campaigns),
+                    id="campaigns",
                 ),
                 Button(
-                    Const("Ваншоты"), on_click=..., id="mainmenucampaigns"
+                    Const("Ваншоты"),
+                    on_click=lambda c, b, m: m.switch_to(MainMenu.oneshots),
+                    id="oneshots",
                 ),
                 Button(
-                    Const("Академия"), on_click=..., id="mainmenucampaigns"
+                    Const("Академия"),
+                    on_click=lambda c, b, m: m.switch_to(MainMenu.academy),
+                    id="academy",
                 ),
             ),
             state=MainMenu.main,
-        )
+        ),
+        Window(
+            Const("Это меню кампаний, к которым у вас есть доступ"),
+            Column(
+                # here go some of the companies
+                Button(
+                    Const("Назад"),
+                    id="back_to_menu",
+                    on_click=lambda c, b, m: m.switch_to(MainMenu.main),
+                ),
+            ),
+            state=MainMenu.campaigns,
+        ),
+        Window(
+            Const("Это меню ваншотов, к которым у вас есть доступ"),
+            Column(
+                # here go some of the companies
+                Button(
+                    Const("Назад"),
+                    id="back_to_menu",
+                    on_click=lambda c, b, m: m.switch_to(MainMenu.main),
+                ),
+            ),
+            state=MainMenu.oneshots,
+        ),
+        Window(
+            Const("Это меню АКАДЕМИИ!!"),
+            Column(
+                # here go some of the companies
+                Button(
+                    Const("Назад"),
+                    id="back_to_menu",
+                    on_click=lambda c, b, m: m.switch_to(MainMenu.main),
+                ),
+            ),
+            state=MainMenu.academy,
+        ),
     )
 )
--- bot/states/mainmenu.py
+++ bot/states/mainmenu.py
@@ -3,3 +3,6 @@ from aiogram.fsm.state import StatesGroup, State
 
 class MainMenu(StatesGroup):
     main = State()
+    campaigns = State()
+    oneshots = State()
+    academy = State()
